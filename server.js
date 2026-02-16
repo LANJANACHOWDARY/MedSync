829281aed6f00be3f66a36fd860d5240360b2c16
Poojaa <rpoojaa06@gmail.com>
2026-02-16 16:06:21 +0530

    Backend updates

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'anjana', // Your password from the screenshot
+    password: 'root', 
     database: 'medsync'
 });
 
